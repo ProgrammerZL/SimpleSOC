bf1f985f94eee0eac29a36d621014b0ec9b129ec
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-22 09:51:46 +0800

    Update PC.v

--- S_c_datapath/PC.v
+++ S_c_datapath/PC.v
@@ -1,1 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:47:39 03/22/2021 
+// Design Name:    Zezhong Wang
+// Module Name:    PC 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 
+module PC(
+    clk,
+    input_pc,
+    output_pc,
+    rst_n
+);
+  
+  reg   [31:0]  output_pc;
+  
+  always @(posedge clk or negedge rst_n)
+    begin
+      if(~rst_n == 1)
+        begin
+          output_pc <= 32'b0;
+        end
+      else
+        begin
+          output_pc <= input_pc;
+        end
+    end
+endmodule
