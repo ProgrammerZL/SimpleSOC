0efb3d05cd2d06d080c04ed9e21417f992344855
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-23 17:14:47 +0800

    Update and rename AND_GATE.v to ANDGate.v

---
+++ S_c_datapath/ANDGate.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:05:12 03/23/2015 
+// Design Name:    Zezhong Wang
+// Module Name:    ANDGate 
+// Project Name:  
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module ANDGate(Branch, Zero, and_out);
+  
+  input Branch;
+  input Zero;
+  output and_out;
+  
+
+  always @(*)
+    begin
+      and_out <= Branch && Zero;
+    end
+endmodule
