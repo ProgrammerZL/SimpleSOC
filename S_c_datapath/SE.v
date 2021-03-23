44275b4d293f819789e65713ba440a93c4c01896
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-23 15:09:56 +0800

    Create SE.v

---
+++ S_c_datapath/SE.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:06:41 03/23/2015 
+// Design Name:    Zezhong Wang
+// Module Name:    SE 
+// Project Name:  
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module SE(
+    instr,
+    ext_result
+);
+  
+  input [15:0]  instr;
+  output reg [31:0] ext_result;
+  
+  always @(instr)
+    begin
+      ext_result[31:0] <= instr[15:0];
+    end
+endmodule
