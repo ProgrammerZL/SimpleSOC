362e8783be6643e7f0f3326cfdc7e85dcbcefd2c
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-23 15:04:15 +0800

    Update ALUCtrl.v

--- S_c_datapath/ALUCtrl.v
+++ S_c_datapath/ALUCtrl.v
@@ -26,28 +26,29 @@ module ALUCtrl(
 );
   
   input [1:0]   ALUOp;
-  input [10:0]  OpField;
-  output reg [3:0]  ALUCtrl';
+  input [5:0]  FunCode;
+  output reg [3:0]  ALUCtrl;
   
-  always @(ALUOp or OpField)
+  always @(ALUOp or FunCode)
     begin
       if(ALUOp == 2'b00)
         ALUCtrl <= 4'b0010; // Load and Store Used Type
       else if(ALUOp == 2'b01)
-        ALUCtrl <= 4'b0111; // Branch Used Type
+        ALUCtrl <= 4'b0110; // Branch Used Type
       else if(ALUOp == 2'b10)
         begin
-          if(OpField == 11'b10001011000)
-            ALUCtrl <= 4'b0010; // Add
-          else if(OpField == 11'b11001011000)
-            ALUCtrl <= 4'b0110; // Subtract
-          else if(OpField == 11'b11001010000)
-            ALUCtrl <= 4'b0000; // AND
-          else if(OpField == 11'b10101010000)
-            ALUCtrl <= 4'b0001; // OR
-          else if(OpField == 11'b10001011001)
-            ALUCtrl <= 4'b1100; // NOR
+              if(FunCode == 6'b100100);
+                    ALUCtrl <= 4'b0000; // AND
+              else if(FunCode == 6'b100101)
+                    ALUCtrl <= 4'b0001; // OR
+              else if(FunCode == 6'b100000)
+                    ALUCtrl <= 4'b0010; // ADD
+              else if(FunCode == 6'b100010)
+                    ALUCtrl <= 4'b0110; // SUBTRACT
+              else if(FunCode == 6'b101010)
+                    ALUCtrl <= 4'b0111; // SLT
+              else if(FunCode == 6'b100111)
+                    ALUCtrl <= 4'b1100;           
         end
     end
 endmodule
-          
