68bbbbff3a4dce6a99f85ff745a6d516e47341c0
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-27 11:01:50 +0800

    Update CPU.v

--- S_c_datapath/CPU.v
+++ S_c_datapath/CPU.v
@@ -18,3 +18,45 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+module CPU(
+      clk,
+      rst_n,
+      pc_in,
+      pc_out,
+      r_addr,
+      instr,
+      instr1,
+      instr2,
+      RegDst,
+      w_reg,
+     
+  
+);
+  
+  input clk;
+  input rst_n;
+  
+// Connect to Program Counter
+  output wire [31:0] pc_in, pc_out;
+  PC pc_0(
+    //input
+    .clk(clk),
+    .rst_n(rst_n),
+    .pc_in(pc_in),
+    //outputs
+    .pc_out(pc_out)
+  );
+  
+  // Connect to Instruction Memory
+  output wire [31:0] instr;
+  IM im_0(
+      //input
+    .clk(clk),
+    .r_addr(pc_out),
+    //output
+    .instr(instr)
+  );
+  
+  // Connect to RegMUX which is between Register File and Instruction Memory
+  output wire [4:0] w_reg;
+  RegMux
