7574146cdf730e3be60650b4e20ca64b99eae25a
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-27 10:47:19 +0800

    Update CPU.v

--- S_c_datapath/CPU.v
+++ S_c_datapath/CPU.v
@@ -1,1 +1,20 @@
-
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:47:32 03/27/2021 
+// Design Name:    Zezhong Wang
+// Module Name:    CPU.v 
+// Project Name:  
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
