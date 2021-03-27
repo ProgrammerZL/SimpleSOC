ff10cc2b34544a31ea5e6a256e8a1d981dfe92a7
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-27 14:16:51 +0800

    Update ALUCtrl.v

--- S_c_datapath/ALUCtrl.v
+++ S_c_datapath/ALUCtrl.v
@@ -25,30 +25,30 @@ module ALUCtrl(
       ALUCtrl
 );
   
-  input [1:0]   ALUOp;
-  input [5:0]  FunCode;
-  output reg [3:0]  ALUCtrl;
+      input [1:0]   ALUOp;
+      input [5:0]   OpField;
+      output reg [3:0]  ALUCtrl;
   
-  always @(ALUOp or FunCode)
-    begin
-      if(ALUOp == 2'b00)
-        ALUCtrl <= 4'b0010; // Load and Store Used Type
-      else if(ALUOp == 2'b01)
-        ALUCtrl <= 4'b0110; // Branch Used Type
-      else if(ALUOp == 2'b10)
-        begin
-              if(FunCode == 6'b100100);
-                    ALUCtrl <= 4'b0000; // AND
-              else if(FunCode == 6'b100101)
-                    ALUCtrl <= 4'b0001; // OR
-              else if(FunCode == 6'b100000)
-                    ALUCtrl <= 4'b0010; // ADD
-              else if(FunCode == 6'b100010)
-                    ALUCtrl <= 4'b0110; // SUBTRACT
-              else if(FunCode == 6'b101010)
-                    ALUCtrl <= 4'b0111; // SLT
-              else if(FunCode == 6'b100111)
-                    ALUCtrl <= 4'b1100;           
-        end
-    end
+      always @(ALUOp or OpField)
+      begin
+            if(ALUOp == 2'b00)
+            ALUCtrl <= 4'b0010; // Load and Store Used Type
+            else if(ALUOp == 2'b01)
+            ALUCtrl <= 4'b0110; // Branch Used Type
+            else if(ALUOp == 2'b10)
+             begin
+                   if(OpField == 6'b100100);
+                        ALUCtrl <= 4'b0000; // AND
+                   else if(OpField == 6'b100101)
+                        ALUCtrl <= 4'b0001; // OR
+                   else if(OpField == 6'b100000)
+                        ALUCtrl <= 4'b0010; // ADD
+                   else if(OpField == 6'b100010)
+                        ALUCtrl <= 4'b0110; // SUBTRACT
+                   else if(OpField == 6'b101010)
+                        ALUCtrl <= 4'b0111; // SLT
+                   else if(OpField == 6'b100111)
+                        ALUCtrl <= 4'b1100;           
+             end
+      end
 endmodule
