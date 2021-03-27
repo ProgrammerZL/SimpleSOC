d5192e95a001abcf20e7bd11c8629f2c02cb4b7a
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-27 14:20:19 +0800

    Update ALUCtrl.v

--- S_c_datapath/ALUCtrl.v
+++ S_c_datapath/ALUCtrl.v
@@ -20,35 +20,35 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module ALUCtrl(
-      ALUOp,
-      OpField,
-      ALUCtrl
+      aluop,
+      opfield,
+      aluctrl
 );
   
-      input [1:0]   ALUOp;
-      input [5:0]   OpField;
-      output reg [3:0]  ALUCtrl;
+      input [1:0]   aluop;
+      input [5:0]   opfield
+      output reg [3:0]  aluctrl;
   
-      always @(ALUOp or OpField)
+      always @(aluop or opfield)
       begin
-            if(ALUOp == 2'b00)
-            ALUCtrl <= 4'b0010; // Load and Store Used Type
-            else if(ALUOp == 2'b01)
-            ALUCtrl <= 4'b0110; // Branch Used Type
-            else if(ALUOp == 2'b10)
+            if(aluop == 2'b00)
+            aluctrl <= 4'b0010; // Load and Store Used Type
+            else if(aluop == 2'b01)
+            aluctrl <= 4'b0110; // Branch Used Type
+            else if(aluop == 2'b10)
              begin
-                   if(OpField == 6'b100100);
-                        ALUCtrl <= 4'b0000; // AND
-                   else if(OpField == 6'b100101)
-                        ALUCtrl <= 4'b0001; // OR
-                   else if(OpField == 6'b100000)
-                        ALUCtrl <= 4'b0010; // ADD
-                   else if(OpField == 6'b100010)
-                        ALUCtrl <= 4'b0110; // SUBTRACT
-                   else if(OpField == 6'b101010)
-                        ALUCtrl <= 4'b0111; // SLT
-                   else if(OpField == 6'b100111)
-                        ALUCtrl <= 4'b1100;           
+                   if(opfield == 6'b100100);
+                        opfield <= 4'b0000; // AND
+                   else if(opfield == 6'b100101)
+                        aluctrl <= 4'b0001; // OR
+                   else if(opfield == 6'b100000)
+                        aluctrl <= 4'b0010; // ADD
+                   else if(opfield == 6'b100010)
+                        aluctrl <= 4'b0110; // SUBTRACT
+                   else if(opfield == 6'b101010)
+                        aluctrl <= 4'b0111; // SLT
+                   else if(opfield == 6'b100111)
+                        aluctrl <= 4'b1100;           
              end
       end
 endmodule
