4d471dccbb70b86d5417fb0cee878c90228c1c8b
ZZhong <43017553+wzezhong@users.noreply.github.com>
2021-03-27 10:51:20 +0800

    Update RegMUX.v

--- S_c_datapath/RegMUX.v
+++ S_c_datapath/RegMUX.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    16:33:02 03/23/2015 
+// Create Date:    16:33:02 03/23/2021 
 // Design Name:    Zezhong Wang
 // Module Name:    RegMUX 
 // Project Name:  
